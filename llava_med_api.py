f63c4974650f4e4e0a4b27febc24a6219d6fc852
argasam <18220023@std.stei.itb.ac.id>
2024-09-06 04:16:27 +0000

    "add translation"

--- llava_med_api.py
+++ llava_med_api.py
@@ -9,6 +9,8 @@ import json
 import hashlib
 import logging
 import re
+from google.cloud import translate
+
 
 app = FastAPI()
 
@@ -27,6 +29,34 @@ app.add_middleware(
 
 CONTROLLER_ADDRESS = "http://localhost:10000"  # Replace with your controller's address
 WORKER_TIMEOUT = 60.0  # Increased timeout
+
+# Initialize Translation client
+def translate_text(
+    text: str, project_id: str = "apt-market-430913-t8"
+    ):
+
+    client = translate.TranslationServiceClient()
+
+    location = "global"
+
+    parent = f"projects/{project_id}/locations/{location}"
+
+    # Translate text from English to French
+    # Detail on supported types can be found here:
+    # https://cloud.google.com/translate/docs/supported-formats
+    response = client.translate_text(
+        request={
+            "parent": parent,
+            "contents": [text],
+            "mime_type": "text/plain",  # mime types: text/plain, text/html
+            "source_language_code": "en-US",
+            "target_language_code": "id",
+        }
+    )
+
+    # Display the translation for each input text provided
+    for translation in response.translations:
+        return translation.translated_text
 
 async def get_worker_address(model: str) -> str:
     async with httpx.AsyncClient() as client:
@@ -134,10 +164,11 @@ async def generate(image: UploadFile = File(...), prompt: str = Form(...)):
             
             # Clean up the response
             generated_text = full_text.replace(f"[INST] <image>\n{prompt} [/INST]", "").strip()
+            translated_text = translate_text(generated_text)
             
             logger.info(f"Final generated text: {generated_text}")
             
-            return {"generated_text": generated_text}
+            return {"generated_text": translated_text}
 
     except httpx.RequestError as e:
         logger.error(f"Network error occurred: {str(e)}")
